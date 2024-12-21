26d1296bbf8782c197f320abc633d05457e970fc
Guechtouli Anis <guechtoulianiss7@gmail.com>
2024-12-21 01:40:10 +0100

    shot_frame_to_df

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
-from pyspark.sql.functions import col, regexp_extract, when,round
-from pyspark.sql.functions import sqrt, pow, lit
+from pyspark.sql.functions import col, regexp_extract,round, sqrt, pow, lit
+import pandas as pd
+import ast
 
 # Function to split the location column into x and y coordinates
 def split_location(df):
@@ -21,6 +22,19 @@ def distance_to_goal(df):
 # Function to calculate the angle to the goal
 
 # Function to export shot_freeze_frame to a separate dataframe
+def shot_frame_to_df(df):
+    processed_rows = []
+    for row in df.collect():
+        id = row.id
+        shot_frame = row.shot_freeze_frame.split('}, {')
+        for i in range(len(shot_frame)):
+            x, y = ast.literal_eval(shot_frame[i].split('},')[0].split(': ')[1].split('],')[0] + ']')
+            position = shot_frame[i].split('},')[1].split(': ')[3].strip("''")
+            teammate = 'True' if 'True' in shot_frame[i].split('},')[2] else 'False'
+            processed_rows.append({'Shot_id': id, 'x': x, 'y': y, 'position': position, 'teammate': teammate})
+    
+    df = pd.DataFrame(processed_rows)
+    return df
 
 # Function to calculate the number of defenders between the shooter and the goal
 
