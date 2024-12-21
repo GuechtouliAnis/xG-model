9e47bdc1b7d7b6e3e64f4e9b678e9db3856dc653
Guechtouli Anis <guechtoulianiss7@gmail.com>
2024-12-21 16:11:38 +0100

    favorite foot function

--- pp_events.py
+++ pp_events.py
@@ -1,34 +1,77 @@
-from pyspark.sql.functions import col, regexp_extract,round, sqrt, pow, lit,udf
+from pyspark.sql.functions import col, regexp_extract,round, sqrt, pow, lit,udf,when
 from pyspark.sql.types import FloatType
 import pandas as pd
 import ast
 import math
 
-# Function to split the location column into x and y coordinates
+######### Function to split the location column into x and y coordinates #########
 def split_location(df):
     df_l = df.withColumn("shot_location_x", regexp_extract(col("location"), r'\[(.*?),', 1).cast("float")) \
              .withColumn("shot_location_y", regexp_extract(col("location"), r', (.*?)\]', 1).cast("float")).drop('location')
     return df_l
 
-# Function to calculate the distance to the goal
+######### Function to calculate the distance to the goal #########
 def distance_to_goal(df):
     goal_x, goal_y = 120, 40
     df = df.withColumn("distance_to_goal",
                        round(sqrt(pow(df.shot_location_x - lit(goal_x), 2) + pow(df.shot_location_y - lit(goal_y), 2)),4))
     return df
 
-# Function to export shot_freeze_frame dataframe [id, shot_freeze_frame]
+######### Function to export shot_freeze_frame dataframe [id, shot_freeze_frame] #########
 def shot_freeze_frame(df):
     df = df.select('id', 'shot_freeze_frame').dropna(subset=['shot_freeze_frame'])
     return df
 
-# Function to export shot data
+######### Function to get the favorite foot of the shooter #########
+def preferred_foot(events):
+    # Filter Pass and Shot events, selecting relevant columns
+    pass_bp = events.filter(events.type == 'Pass')\
+        .select('player_id', col('pass_body_part').alias('body_part'))\
+        .filter(col('body_part').isin('Right Foot', 'Left Foot'))
+    
+    shot_bp = events.filter(events.type == 'Shot')\
+        .select('player_id', col('shot_body_part').alias('body_part'))\
+        .filter(col('body_part').isin('Right Foot', 'Left Foot'))
 
-# Function to export pass data
+    # Union the two DataFrames
+    bp = pass_bp.union(shot_bp)
 
-# Function to calculate the angle to the goal
+    # Map 'body_part' into separate columns for left and right foot counts
+    bp_mapped = bp.withColumn('left_foot', (col('body_part') == 'Left Foot').cast('int'))\
+        .withColumn('right_foot', (col('body_part') == 'Right Foot').cast('int'))\
+        .drop('body_part')
 
-# Function to calculate the angle
+    # Group by player and calculate sums for left and right foot counts
+    foot_counts = bp_mapped.groupBy('player_id')\
+        .sum('left_foot', 'right_foot')\
+        .withColumnRenamed('sum(left_foot)', 'left_foot')\
+        .withColumnRenamed('sum(right_foot)', 'right_foot')
+
+    # Add total actions column
+    foot_counts = foot_counts.withColumn("total_actions", col("left_foot") + col("right_foot"))
+
+    # Determine preferred foot based on the 66% rule
+    foot_counts = foot_counts.withColumn(
+        "preferred_foot",
+        when((col("left_foot") / col("total_actions")) >= 0.66, "Left Foot")
+        .when((col("right_foot") / col("total_actions")) >= 0.66, "Right Foot")
+        .otherwise("Two-Footed"))
+
+    return foot_counts.drop('left_foot', 'right_foot', 'total_actions')
+
+######### Function to export shot data #########
+def shot_data(df):
+    # Only shots
+    # 5th period is penalties
+    # set shot_freeze_frame to null when it's penalty
+    
+    columns = []
+    df = df.filter(df.type=='Shot' & df.period < 5).select(columns)
+    return df
+
+######### Function to export pass data #########
+
+######### Function to calculate the angle to the goal #########
 def calculate_shot_angle(shot_x, shot_y):
     goal_x1, goal_y1 = 120, 36  # Left post
     goal_x2, goal_y2 = 120, 44  # Right post
@@ -61,7 +104,7 @@ def get_shot_angle(df):
         calculate_shot_angle_udf(df["shot_location_x"], df["shot_location_y"]))
     return df
 
-# Function to export shot_freeze_frame to a separate dataframe
+######### Function to export shot_freeze_frame to a separate dataframe #########
 def shot_frame_to_df(df):
     processed_rows = []
     for row in df.collect():
@@ -80,10 +123,10 @@ def shot_frame_to_df(df):
 
 # Function to calculate the number of players inside the area of shooting
 
-# Function to get the favorite foot of the shooter
-
 # Function to get the position that the shooter plays
 
 # Function to add a multiplier according to the position of the shooter
 
 # Function to get pass information
+
+# Functions to create visualizations
